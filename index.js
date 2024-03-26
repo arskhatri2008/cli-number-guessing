b6b728a3e9b24ae9930654a0a8e1c529117dbe4f
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-03-26 18:00:05 +0500

    update

--- index.js
+++ index.js
@@ -2,27 +2,24 @@ import inquirer from "inquirer";
 // computer will generate a random number
 // user will guess the number
 // compare user input with computer generated number and show the result
-const randomNumber = 13;
-// const randomNumber = Math.floor(Math.random() * 10) + 1;
-// console.log(randomNumber)
-// console.log("Guess the number between 1 to 10")
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 // user input
-// inquirer.prompt([
-//     {
-//         name: "userGuessNumber",
-//         type: "number",
-//         message: "Enter your guess number"
-//     }
 const answers = await inquirer.prompt([
     {
         name: "userGuessNumber",
         type: "number",
-        message: "Enter your guess number"
+        message: "Please guess the number between 1 to 10"
     }
 ]);
 if (answers.userGuessNumber === randomNumber) {
     console.log("Congratulations! you guess the right number");
 }
+else if (answers.userGuessNumber > randomNumber) {
+    console.log("You guess the wrong number. The right number was: ", randomNumber);
+}
+else if (answers.userGuessNumber < randomNumber) {
+    console.log("You guess the wrong number. The right number was: ", randomNumber);
+}
 else {
-    console.log("You guess the wrong number");
+    console.log("Please enter a valid number");
 }
